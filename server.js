b5658739c6dbf5f49c7baadd47f46f0a8c3845b8
Danny Oka <dannyoka@gmail.com>
2020-09-21 21:30:38 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+});
